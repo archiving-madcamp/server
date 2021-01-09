da1c67d4b8103dd356c95a7a68ada4f7b1c03edc
Jaegoomon <jaegoo1199@gmail.com>
2021-01-09 02:47:40 +0000

    video chat app: /

---
+++ plan.md
@@ -0,0 +1,11 @@
+# Plan Of Action
+
+- Initialilze out NodeJS Project (Done)
+- Initialize our first view
+- Create a room id
+- Add tje abo;otu to view our own Video
+- Add ability to allow others to stream their video
+- Add styling 
+- Add the ability to create mesages
+- Add mute button
+- Add stop video button
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    // 메인 도메인에 들어가면 아이디를 할당해줌
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+// io: socket.io packate
+// socket: connection information when connected
+io.on('connection', socket => {
+    // implement event listener
+    // get information from script.js
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    })
+})
+
+server.listen(3000);
