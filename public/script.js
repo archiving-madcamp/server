e7dcb354cdabc58bb3ed6a25d057983ff106aadc
Jaegoomon <jaegoo1199@gmail.com>
2021-01-09 05:59:44 +0000

    peer can access the room

--- public/script.js
+++ public/script.js
@@ -1,46 +1,56 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-// assign unique userId
-const myPeer = new Peer(undefined, {
+var peer = new Peer(undefined, {
+    path: '/peerjs',
     host: '/',
-    port: '3001'
+    port: '3000'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
+// promise
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream)
-
-    myPeer.on('call', call => {
+    peer.on('call', call => {
         call.answer(stream)
+        const video = document.createElement('video')
+        // 상대방의 stream을 추가한다.
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
     })
-
-    // receive event
+    // 이벤트를 보냄
     socket.on('user-connected', userId => {
+        console.log("비디오를 킵니다. " + userId)
         connectToNewUser(userId, stream)
     })
-})
+}).catch(err => console.log(err))
 
-myPeer.on('open', id => {
-    // sending event
+// peer가 들어왔을 때 관련 정보를 보냄
+peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
+    console.log('입장하였습니다.')
 })
 
-function connectToNewUser(userId, stream) {
-    const call = myPeer.call(userId, stream)
+// peer가 연결되었을 때
+const connectToNewUser = (userId, stream) => {
+    console.log(userId)
+    // 상대방을 부른다.
+    const call = peer.call(userId, stream)
     const video = document.createElement('video')
+    // 상대방의 stream을 내 화면에 추가한다.
+    console.log("화면을 추가합니다.")
     call.on('stream', userVideoStream => {
+        console.log("화면 추가 함수를 실행")
         addVideoStream(video, userVideoStream)
-    })
-    call.on('close', () => {
-        video.remove()
     })
 }
 
-function addVideoStream(video, stream) {
+const addVideoStream = (video, stream) => {
+    console.log("화면 추가")
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid')
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use('/peerjs', peerServer)
 
 app.get('/', (req, res) => {
     // 메인 도메인에 들어가면 아이디를 할당해줌
@@ -24,6 +29,7 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         console.log(roomId, userId)
         socket.join(roomId)
+        //send information about userId
         socket.to(roomId).broadcast.emit('user-connected', userId)
     })
 })
